fd839ed5089e772b6f671f695e514c81566a7a98
KavinilaL <kavinilaloganathan@gmail.com>
2024-07-28 10:13:01 +0530

    Create CALCULATOR.py

---
+++ CALCULATOR.py
@@ -0,0 +1,13 @@
+
+a=int(input("Enter a number:"))
+b=input("Enter an operator:")
+c=int(input("Enter a number"))
+
+if b=="+":
+    print("the addition of", a, "and", c, "is", a+c)
+if b=="-":
+    print("the subraction of ", c, "from", a, "and", c, "is", a-c)
+if b=="*":
+    print("the multipilication of", a, "and", c, "is", a*c)
+if b=="/":
+    print("the division of", a, "by", c, "is", a/c)
